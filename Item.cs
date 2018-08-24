0194d176e0ad2b6c9a7df6c2100f2030f671f9ca
allef brenno <allefbrenno100@hotmail.com>
2018-08-24 15:57:41 -0300

    Item Coin and Documents Code

---
+++ Item.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    private AudioSource source;
+    public AudioClip itemSound;
+    public float vol;
+
+	void Awake ()
+    {
+		source = GetComponent<AudioSource> ();
+	}
+
+	/*Adiciona o som escolhido na colisão */
+
+	 void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            source.PlayOneShot(itemSound, vol);
+            /*Faz o item sumir caso entre em contato com um player*/
+            Destroy(GetComponent<BoxCollider>());
+            Destroy(GetComponent<MeshRenderer>());
+            Destroy(gameObject, 2);
+        }
+	}
+}
+
